f85d1274541cbea7547440b4d31d9e3f7219792c
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2026-03-12 15:49:12 -0600

    feat(shaders): add constants.glsl with PI, INV_PI, EPSILON

---
+++ shaders/common/constants.glsl
@@ -0,0 +1,15 @@
+/**
+ * @file constants.glsl
+ * @brief Mathematical constants for PBR lighting calculations.
+ *
+ * Pure constants, no dependencies.
+ */
+
+#ifndef CONSTANTS_GLSL
+#define CONSTANTS_GLSL
+
+const float PI        = 3.14159265358979323846;
+const float INV_PI    = 0.31830988618379067154;
+const float EPSILON   = 1e-5;
+
+#endif // CONSTANTS_GLSL
