5529ea8d957a430673285110cfb5e4d43811bdf5
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2026-03-13 18:03:29 -0600

    feat(shaders): extract shared math constants into common/constants.glsl
    Create constants.glsl (PI, TWO_PI, HALF_PI, INV_PI, EPSILON) and
    replace inline PI definitions in the four IBL compute shaders.

---
+++ shaders/common/constants.glsl
@@ -0,0 +1,15 @@
+/**
+ * @file constants.glsl
+ * @brief Mathematical constants shared across all shaders.
+ */
+
+#ifndef CONSTANTS_GLSL
+#define CONSTANTS_GLSL
+
+const float PI         = 3.14159265358979323846;
+const float TWO_PI     = 6.28318530717958647692;
+const float HALF_PI    = 1.57079632679489661923;
+const float INV_PI     = 0.31830988618379067154;
+const float EPSILON    = 0.0001;
+
+#endif // CONSTANTS_GLSL
